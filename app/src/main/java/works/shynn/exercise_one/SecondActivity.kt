5431f5184eeebe866eeb89fe8413b8cfdd3b05ac
shynnhuy <shynn@hotmail.com>
2020-07-03 16:56:01 +0700

    Add relative design

--- app/src/main/java/works/shynn/exercise_one/SecondActivity.kt
+++ app/src/main/java/works/shynn/exercise_one/SecondActivity.kt
@@ -3,15 +3,15 @@ package works.shynn.exercise_one
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
 
-        toRelative.setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
+        toLinear.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
